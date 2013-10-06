8a33f190b50a5d578fe52d1dc50e2850eaa2bf65
Ben Lindsey <ben.lindsey@gmail.com>
2013-10-06 01:10:30 -0700

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Ben Lindsey on 10/6/13.
+//  Copyright (c) 2013 Ben Lindsey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
